9429eb485f75570362576521d8660df9f30a1980
Jianli-Huang <huangjianli_st@163.com>
2023-11-05 16:31:14 +0800

    Add core and client log by using spdlog.

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,10 +7,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv)
 {
 	printf("1111\n");
-	//Hazel::Log::Init();
-	//HZ_CORE_WARN("Initialized Log!");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
 	int a = 4;
-	//HZ_INFO("Hello var= {0}", a);
+	HZ_INFO("Hello var= {0}", a);
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
