e42fc498a386a37ce1d689c07c9442784f269f8a
Jianli-Huang <huangjianli_st@163.com>
2023-11-11 19:28:22 +0800

    add application mouse key event

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Hazel {
@@ -12,6 +13,11 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+	
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 	
 	private:
 		std::unique_ptr<Window> m_Window;
