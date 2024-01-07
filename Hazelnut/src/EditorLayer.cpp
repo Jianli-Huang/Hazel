f19ed90e33c5c3955bd652725b68a759a307c59f
Jianli-Huang <huangjianli_st@163.com>
2024-01-07 15:21:36 +0800

    add SceneHierarchyPanel

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -64,6 +64,9 @@ namespace Hazel
 		};
 
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
+		m_SecondCamera.AddComponent<NativeScriptComponent>().Bind<CameraController>();
+
+		m_SceneHierarchyPanel.SetContext(m_ActiveScene);
 	}
 
 	void EditorLayer::OnDetach()
@@ -166,6 +169,8 @@ namespace Hazel
 			}
 			ImGui::EndMenuBar();
 		}
+
+		m_SceneHierarchyPanel.OnImGuiRender();
 
 		ImGui::Begin("Settings");
 		auto stats = Hazel::Renderer2D::GetStats();
