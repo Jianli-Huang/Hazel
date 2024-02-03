b8ea247968d7bd9b2ba477b901ffed9dc4a4263d
Jianli-Huang <huangjianli_st@163.com>
2024-02-03 14:43:13 +0800

    add content browser imgui drag drop

--- Hazelnut/src/Panels/ContentBrowserPanel.h
+++ Hazelnut/src/Panels/ContentBrowserPanel.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Hazel/Renderer/Texture.h"
+
 #include <filesystem>
 
 namespace Hazel
@@ -12,5 +14,6 @@ namespace Hazel
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_CurrentDirectory;
+		Ref<Texture2D> m_DirectoryIcon, m_FileIcon;
 	};
 }
